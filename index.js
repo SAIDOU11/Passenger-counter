3289122d807e37a4d3dd9ef77f60779ab29f2047
Salif Dieye Saidou <104588234+SAIDOU11@users.noreply.github.com>
2022-11-30 11:26:34 +0100

    Passenger counter app

---
+++ index.js
@@ -0,0 +1,20 @@
+// 1. Grab the save-el paragrah and store it in a variable called saveEl
+let countEl = document.getElementById("count-el");
+let count = 0;
+let saveEl = document.getElementById("save-el");
+console.log(saveEl);
+
+function increment() {
+  count += 1;
+  countEl.textContent = count;
+}
+
+function save() {
+  // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
+  // 3. Render the variable in the saveEl using innerText
+  // NB: Make sure to not delete the existing content of the paragraph
+
+  let countStr = count + " - ";
+  saveEl.textContent += countStr;
+  console.log(count);
+}
